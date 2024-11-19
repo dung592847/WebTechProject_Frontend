0023f0fa2639e756d95ad5f3a0fe8e25f44f01a5
Dung <s0592847@htw-berlin.de>
2024-11-19 22:26:19 +0100

    added SeatAndContactForm

---
+++ webtech-frontend/src/components/SeatAndContactForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="seat-and-contact-form">
+      <div class="row">
+        <div class="col-md-6">
+          <AirplaneSeats />
+        </div>
+        <div class="col-md-6">
+          <Contactform />
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  import AirplaneSeats from '@/components/AirplaneSeats.vue';
+  import Contactform from '@/components/Contactform.vue';
+
+  export default {
+    name: 'SeatAndContactForm',
+    components: {
+      AirplaneSeats,
+      Contactform,
+    },
+  };
+  </script>
+
+  <style scoped>
+  .row {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .col-md-6 {
+    flex: 0 0 50%;
+    max-width: 50%;
+    padding: 10px; /* optional, falls Abstände nötig sind */
+  }
+  </style>
--- webtech-frontend/src/views/FlightDetailsView.vue
+++ webtech-frontend/src/views/FlightDetailsView.vue
@@ -1,29 +1,26 @@
 <template>
-    <NavHeader></NavHeader>
-    <SearchResultBox></SearchResultBox>
-    <AirplaneSeats></AirplaneSeats>
-    <Contactform></Contactform>
-  <FooterComponent></FooterComponent>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-    import NavHeader from "../components/NavHeader.vue"
-    import FooterComponent from "../components/FooterComponent.vue"
-    import AirplaneSeats from "@/components/AirplaneSeats.vue";
-    import Contactform from "@/components/Contactform.vue";
-    import SearchResultBox from "../components/SearchResultBox.vue"
+  <NavHeader></NavHeader>
+  <SearchResultBox></SearchResultBox>
+  <SeatAndContactForm></SeatAndContactForm>
+<FooterComponent></FooterComponent>
+</template>
 
-  
-  export default {
-    
-    name: 'HomeView',
-    components: {
-      NavHeader,
-      AirplaneSeats,
-      Contactform,
-      FooterComponent,
-      SearchResultBox
-    }
+<script>
+// @ is an alias to /src
+  import NavHeader from "../components/NavHeader.vue"
+  import FooterComponent from "../components/FooterComponent.vue"
+  import SeatAndContactForm from "@/components/SeatAndContactForm.vue";
+  import SearchResultBox from "../components/SearchResultBox.vue"
+
+
+export default {
+
+  name: 'HomeView',
+  components: {
+    NavHeader,
+    SeatAndContactForm,
+    FooterComponent,
+    SearchResultBox
   }
-  </script>
+}
+</script>
